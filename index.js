c2c6fead76675db221f51c4bd3bdce32237b83cb
Roger Yeh <ryeh@walmartlabs.com>
2021-12-19 12:23:58 -0800

    update

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require("express");
+const authMiddleware = require("./middleware/auth");
+
+const { MongoClient } = require("mongodb");
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
+const connectionString = 'mongodb+srv://theyehs:Win2000!@395mallard.ssll8.mongodb.net/';
+(async () => {
+    let client = await MongoClient.connect(connectionString,
+        { useNewUrlParser: true });
+
+    app.listen(3000, () => {
+        console.log("Server running on port 3000");
+    });
+})();
+
+const middlewares = [authMiddleware];
+// TODO: add authentication
 
 /**
  * TASK
  * /task/list:
  * /task/addList
  */
-app.get("/task", (req, res, next) => {
+app.get("/task", middlewares, (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });
